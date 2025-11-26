b139751ed25563db312fb43cbcef9975547f6d3e
Stu <stuartgordonsmith5@gmail.com>
2025-11-26 11:53:30 +0000

    added redux and toolkit, created ui and cart stores, imoported to central store, wrapped app in provider, created dummy producst and looped through for more variety, added logic to hide and show cart as well as ability to add and remove items from products component and increase and decrease from cart

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,10 +1,19 @@
+import { uiActions } from '../../store/ui';
 import classes from './CartButton.module.css';
+import { useDispatch, useSelector } from 'react-redux';
 
 const CartButton = (props) => {
+  const totalQuantity = useSelector(state => state.cart.totalQuantity);
+  const dispatch = useDispatch();
+
+  const toggleShowCartHandler = () => {
+    dispatch(uiActions.toggleShowCart());
+  }
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleShowCartHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{totalQuantity}</span>
     </button>
   );
 };
--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,41 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const PRODUCTS = [
+  {
+    id: 'p1',
+    title: 'Test',
+    price: 6,
+    description: 'This is a first product - amazing!',
+  },
+  {
+    id: 'p2',
+    title: 'Test 2',
+    price: 5,
+    description: 'This is a second product - even more amazing!',
+  },
+  {
+    id: 'p3',
+    title: 'Test 3',
+    price: 8,
+    description: 'This is a third product - you will love it!',
+  },
+];
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {PRODUCTS.map((product) => (
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );
